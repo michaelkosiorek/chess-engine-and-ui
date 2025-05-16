910926b8f51aa678f4b3cb285169ad8e0c359705
Michael Kosiorek <97412062+michaelkosiorek@users.noreply.github.com>
2025-05-15 22:20:12 -0400

    Moved and changed

--- main.js
+++
@@ -1,6 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-
-app.whenReady().then(() => {
-  const win = new BrowserWindow({ width: 950, height: 800 });
-  win.loadFile('index.html');
-});
