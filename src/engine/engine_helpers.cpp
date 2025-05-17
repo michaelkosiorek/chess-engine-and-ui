c6e1717c7aaf3e91cd63bca454ebdef708de308f
Michael Kosiorek <97412062+michaelkosiorek@users.noreply.github.com>
2025-05-17 19:46:08 -0400

    Introducing gtests
    Haven't used google tests before but I'm trying it because it seems to be pretty smooth and adaptable once implemented. Also forced me to remember how to using makefiles and fully utilize CMake a bit. There are currently barely any tests but its fairly straightforward to add new ones (instructions for myself in newtests.txt)

---
+++ src/engine/engine_helpers.cpp
@@ -0,0 +1,13 @@
+#include "engine_helpers.h"
+
+long long fastPower(int base, int expo) {
+    long long result = 1;
+    while (expo > 0) {
+        if (expo % 2 == 1) { // If expo is odd
+            result *= base;
+        }
+        base *= base;
+        expo /= 2;
+    }
+    return result;
+}
